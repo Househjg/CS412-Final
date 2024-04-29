0fa6afbf308c8004187e5431e5a9437f41e9b7c7
simoncns <simoncns@dukes.jmu.edu>
2024-04-29 13:10:16 -0400

    Assigning input values

--- Max-3Sat.py
+++ Max-3Sat.py
@@ -1,5 +1,13 @@
 def main():
-    return 0
+    variables, n = [int(x) for x in input().split()]
+    print(variables)
+    print(n)
+    clauses = []
+    for _ in range(n):
+        clauses.append(input())
+
+    print(clauses)
+
 
 if __name__ == "__main__":
     main()
