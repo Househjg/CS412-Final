56dc89a23978488025f2fdc2be231d68b3feaef8
simoncns <simoncns@dukes.jmu.edu>
2024-04-29 16:50:06 -0400

    Beginning of Helper Function

--- Max-3Sat.py
+++ Max-3Sat.py
@@ -1,12 +1,16 @@
 def main():
-    variables, n = [int(x) for x in input().split()]
-    print(variables)
-    print(n)
+    num_variables, num_clauses = [int(x) for x in input().split()]
+    print(num_variables)
+    print(num_clauses)
     clauses = []
-    for _ in range(n):
+    for _ in range(num_clauses):
         clauses.append(input())
-
     print(clauses)
+    print(max_3Sat(num_variables, clauses))
+
+def max_3Sat(num_variables, clauses):
+    variables = [True for _ in range(num_variables)]
+    return variables
 
 
 if __name__ == "__main__":
