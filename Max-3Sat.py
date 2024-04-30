3dba7ad5e49c785db63c98a76f52e999924e40c3
simoncns <simoncns@dukes.jmu.edu>
2024-04-30 00:35:17 -0400

    Spacing

--- Max-3Sat.py
+++ Max-3Sat.py
@@ -3,11 +3,14 @@ import itertools
 def main():
     num_variables, num_clauses = [int(x) for x in input().split()]
     clauses = []
+
     for _ in range(num_clauses):
         clause = [int(x) for x in input().split()]
         clauses.append(clause)
+
     variables, max = max_3Sat(num_variables, clauses)
     print(max)
+    
     for i in range(len(variables)):
         if variables[i] > 0:
             result_string = "T"
@@ -19,6 +22,7 @@ def max_3Sat(num_variables, clauses):
     variables = [1 for _ in range(num_variables)]
     max = 0
     all_combinations = itertools.product([1, -1], repeat=num_variables)
+
     for combination in all_combinations:
         clauses_satisfied = max_3Sat_helper(combination, clauses)
         if clauses_satisfied == clauses:
@@ -26,15 +30,18 @@ def max_3Sat(num_variables, clauses):
         elif clauses_satisfied > max:
             variables = combination
             max = clauses_satisfied
+
     return variables, max
 
 def max_3Sat_helper(variables, clauses):
     clauses_satisfied = 0
+
     for i in range(len(clauses)):
         for j in range(len(variables)):
             if clauses[i][j] * variables[j] > 0:
                 clauses_satisfied += 1
                 break
+
     return clauses_satisfied
 
 
